be5b9b29e051dbaefec8e62679f84315cedcb3f6
Nicola Piermattei <pierma.nick@live.it>
2020-07-27 09:08:25 +0200

    Initial Commit

---
+++ EsameFinale/Services/DataEntityContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EsameFinale.Services
+{
+    public class DataEntityContext : DbContext
+    {
+        public DataEntityContext(DbContextOptions<DataEntityContext> options) : base(options)
+        {
+
+        }
+    }
+}
